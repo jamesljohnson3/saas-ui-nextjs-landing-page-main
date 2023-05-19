0bfbd04ec36a7d194f4d578cee9b4458409cf9d8
Admin <support@unlimitpotential.com>
2023-05-19 03:39:51 -0400

    ;l

--- components/blocks/projects/featured.jsx
+++ components/blocks/projects/featured.jsx
@@ -5,7 +5,6 @@ import { m, useAnimation } from "framer-motion"
 import { useInView } from 'react-intersection-observer'
 
 import Badges 		from '../../utils/badge.list.util'
-import Icon 		from '../../utils/icon.util'
 
 import css 			from '../../../styles/sections/projects/featured.module.scss'
 import content 		from '../../../content/projects/featured.json'
@@ -48,7 +47,7 @@ export default function FeaturedProject({ content }, index) {
 						<Badges list={stack} block="stack" fullContainer={false} color={false} />
 					</div>
 					<m.div variants={''} className={css.viewProject}>
-						<Icon icon={[ 'fad', 'arrow-right-to-bracket' ]} />
+					
 					</m.div>
 				</div>
 			</div>
