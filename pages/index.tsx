114fb9936a8267505da3a0e6ce62bc30a9368fd8
Admin <support@unlimitpotential.com>
2023-05-19 03:26:22 -0400

    l

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,7 +71,7 @@ const Home: NextPage = () => {
       />
       <Box>
         <HeroSection />
-<Blog/>
+<Blog mediumArticles={undefined}/>
         <HighlightsSection />
 
         <FeaturesSection />
